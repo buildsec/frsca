34face31dc55a0d081c4280a0bf77cc5204700a4
Michael <mlieberman85@gmail.com>
2021-09-09 00:38:28 -0400

    Fix typo in build script not including static target path

--- build_with_ebpf/bad_cargo/build.sh
+++ build_with_ebpf/bad_cargo/build.sh
@@ -1,7 +1,7 @@
 # Build "compromised" hello world program and add base64 to source
 pushd ../attacker_project
 cargo build --release --target x86_64-unknown-linux-musl
-base64 -w0 ./target/release/attacker_project > ../bad_cargo/src/overwrite_binary/base64_attacker_project
+base64 -w0 ./target/x86_64-unknown-linux-musl/release/attacker_project > ../bad_cargo/src/overwrite_binary/base64_attacker_binary
 popd
 
 # Build payload that overwrites built binary and stage it for "compromised" output hijacking cargo source

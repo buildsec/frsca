fa491255222fcb2821b74e305cbc4ac7d2cdc534
Joshua Lucas <joshua.lucas@outlook.com>
2021-10-19 15:28:23 -0500

    updates to helm charts added release annotations and updated install script

--- kubernetes/tekton-resources/demo/update-and-run.sh
+++ kubernetes/tekton-resources/demo/update-and-run.sh
@@ -3,18 +3,20 @@ set -u
 set -e
 
 remove_old_charts() {
-    helm uninstall gatekeeper-template
-    helm uninstall demo
+    helm uninstall gatekeeper-template || true 
+    helm uninstall demo || true
 }
 
 install_new_charts() {
-    helm upgrade -i gatekeeper-template gatekeeper-template
-    helm upgrade -i demo image-verification --values image-verification/values.yaml
+    helm upgrade -i gatekeeper-template gatekeeper-template || true
+    echo "Wait 5 seconds for gatekeeper-constraint-templates to become available"
+    sleep 5
+    helm upgrade -i demo image-verification --values image-verification/values.yaml || true
 }
 
 
 main(){
-    #remove_old_charts "$@"
+    remove_old_charts "$@"
     install_new_charts "$@"
 }
 

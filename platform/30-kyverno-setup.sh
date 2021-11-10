28573f1184f4dd635b2b5058640f73e68e50993b
Parth Patel <parth.psu@gmail.com>
2021-11-10 11:59:33 -0500

    added hack to use main for kyverno

--- platform/30-kyverno-setup.sh
+++ platform/30-kyverno-setup.sh
@@ -1,5 +1,47 @@
 #!/bin/bash
-set -xeuo pipefail
+set -euo pipefail
+<<<<<<< HEAD
+
+USERPUBKEY="-----BEGIN PUBLIC KEY-----
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEhyQCx0E9wQWSFI9ULGwy3BuRklnt
+IqozONbbdbqz11hlRJy9c7SG+hdcFl9jE9uE/dwtuwU2MqU9T/cN0YkWww==
+-----END PUBLIC KEY-----"
+
+GIT_ROOT=$(git rev-parse --show-toplevel)
+KYVERNO_INSTALL_DIR=${GIT_ROOT}/platform/vendor/kyverno/release
+KYVERNO_RESOURCE_DIR=${GIT_ROOT}/resources/kyverno/admission-control-policy
+
+# Define variables.
+C_GREEN='\033[32m'
+C_YELLOW='\033[33m'
+C_RED='\033[31m'
+C_RESET_ALL='\033[0m'
+
+# Wait until pods are ready.
+# $1: namespace, $2: app label
+wait_for_pods () {
+  echo -e "${C_YELLOW}Waiting: $2 pods in $1...${C_RESET_ALL}"
+  kubectl wait --timeout=5m --for=condition=ready pods -l app=$2 -n $1
+}
+=======
+>>>>>>> d75f4b5 (Enhance the Kyverno install)
+
+GIT_ROOT=$(git rev-parse --show-toplevel)
+KYVERNO_INSTALL_DIR=${GIT_ROOT}/platform/vendor/kyverno/release
+KYVERNO_RESOURCE_DIR=${GIT_ROOT}/resources/kyverno/admission-control-policy
+
+# Define variables.
+C_GREEN='\033[32m'
+C_YELLOW='\033[33m'
+C_RED='\033[31m'
+C_RESET_ALL='\033[0m'
+
+# Wait until pods are ready.
+# $1: namespace, $2: app label
+wait_for_pods () {
+  echo -e "${C_YELLOW}Waiting: $2 pods in $1...${C_RESET_ALL}"
+  kubectl wait --timeout=5m --for=condition=ready pods -l app=$2 -n $1
+}
 
 # Update below if you have a different config.json you want to use.
 DOCKER_CONFIG_JSON=$HOME/.docker/config.json
@@ -8,17 +50,30 @@ DOCKER_CONFIG_JSON=$HOME/.docker/config.json
 #   https://nirmata.com/2021/08/12/kubernetes-supply-chain-policy-management-with-cosign-and-kyverno/
 #   Installation: https://kyverno.io/docs/installation/
 
-# Create secrets for kaniko and kubernetes image pull
-kubectl create namespace kyverno --dry-run=client -o yaml | kubectl apply -f -
+echo -e "${C_GREEN}Installing Kyverno...${C_RESET_ALL}"
+kubectl apply -f $KYVERNO_INSTALL_DIR/release.yaml
+# Wait for kyverno deployment to complete
+wait_for_pods kyverno kyverno
 
-# TODO: This should jsut be the normal secret if the kaniko task is updated to correctly use the docker config secret instead of requiring it to be hardcoded as config.json
+echo -e "${C_GREEN}Creating docker config secrets...${C_RESET_ALL}"
+# TODO: This should just be the normal secret if the kaniko task is updated to correctly use the docker config secret instead of requiring it to be hardcoded as config.json
 kubectl create secret generic secret-dockerconfigjson --type=opaque --from-file=config.json=$DOCKER_CONFIG_JSON --dry-run=client -o yaml | kubectl apply -f -
 
 # NOTE: Pull secret needs to exist in both kyverno namespace as well as the tekton task namespace (default in this case)
 kubectl create secret generic regcred --type=kubernetes.io/dockerconfigjson --from-file=.dockerconfigjson=$DOCKER_CONFIG_JSON -n kyverno  --dry-run=client -o yaml | kubectl apply -f -
 kubectl create secret generic regcred --type=kubernetes.io/dockerconfigjson --from-file=.dockerconfigjson=$DOCKER_CONFIG_JSON --dry-run=client -o yaml | kubectl apply -f -
 
-# Assumes helm already installed by previous scripts
-helm repo add kyverno https://kyverno.github.io/kyverno/
-helm repo update
-helm upgrade --install kyverno kyverno/kyverno -n kyverno --create-namespace --set extraArgs="{--webhooktimeout=15,--imagePullSecrets=regcred}"
+### HACK - setting to Jim's personal image repo for kyverno to resolve
+### failed calling webhook "mutate.kyverno.svc-fail" error. Once this image
+### is offically released, this will be removed.
+kubectl set image -n kyverno deploy/kyverno kyverno=ghcr.io/jimbugwadia/kyverno:v1.5.2-rc3-311-g4835157c
+
+echo -e "${C_GREEN}Patching Kyverno deployment...${C_RESET_ALL}"
+kubectl patch \
+  deployment kyverno \
+  -n kyverno \
+  --type json --patch-file ${GIT_ROOT}/platform/components/kyverno/patch_container_args.json
+
+echo -e "${C_GREEN}Creating verify-image admission control policy...${C_RESET_ALL}"
+cue export $KYVERNO_RESOURCE_DIR/admission-control-verify-image-resources.cue -e template -t key="$USERPUBKEY" --out yaml > $KYVERNO_RESOURCE_DIR/admission-control-verify-image-resources.yaml
+kubectl apply -f $KYVERNO_RESOURCE_DIR
---
+++ platform/vendor/vendor_kyverno.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -euo pipefail
+
+GIT_ROOT=$(git rev-parse --show-toplevel)
+KYVERNO_INSTALL_DIR=${GIT_ROOT}/platform/vendor/kyverno/release
+KYVERNO_RESOURCE_DIR=${GIT_ROOT}/resources/kyverno/admission-control-policy
+
+### HACK to work around the latest version of Kyverno not having the updates Jim made for us with configmaps. 
+### Once offically released. We will go back to using the versioned release.
+KYVERNO_URL="https://raw.githubusercontent.com/kyverno/kyverno/main/config/install.yaml"
+
+# # Define variables.
+# KYVERNO_TAG_RELEASE=main
+# KYVERNO_URL="https://raw.githubusercontent.com/kyverno/kyverno/$KYVERNO_TAG_RELEASE/definitions/release/install.yaml"
+# COSIGN_REPOSITORY=ghcr.io/kyverno/signatures
+# PUBKEY=https://raw.githubusercontent.com/kyverno/kyverno/$KYVERNO_TAG_RELEASE/cosign.pub
+
+# KYVERNO_LATEST=`COSIGN_REPOSITORY=$COSIGN_REPOSITORY cosign verify --key $PUBKEY ghcr.io/kyverno/kyverno:$KYVERNO_TAG_RELEASE`
+# KYVERNO_LATEST_STATUS=$?
+# KYVERNOPRE_LATEST=`COSIGN_REPOSITORY=$COSIGN_REPOSITORY cosign verify --key $PUBKEY ghcr.io/kyverno/kyvernopre:$KYVERNO_TAG_RELEASE`
+# KYVERNOPRE_LATEST_STATUS=$?
+# KYVERNOCLI_LATEST=`COSIGN_REPOSITORY=$COSIGN_REPOSITORY cosign verify --key $PUBKEY ghcr.io/kyverno/kyverno-cli:$KYVERNO_TAG_RELEASE`
+# KYVERNOCLI_LATEST_STATUS=$?
+
+# if [ $KYVERNO_LATEST_STATUS -eq 0 ] && [ $KYVERNOPRE_LATEST_STATUS -eq 0 ] && [ $KYVERNOCLI_LATEST_STATUS -eq 0 ] ; then
+#   # Download the release file
+  curl "$KYVERNO_URL" > $KYVERNO_INSTALL_DIR/release.yaml
+#fi

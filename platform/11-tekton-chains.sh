ebd92fb87a54d7b578974a64b22eb7d96e7979d4
rjulian <richard@rjulian.net>
2021-11-13 17:11:59 -0800

    Linting with shellcheck

--- platform/11-tekton-chains.sh
+++ platform/11-tekton-chains.sh
@@ -11,15 +11,15 @@ GIT_ROOT=$(git rev-parse --show-toplevel)
 #     Possibles values: `intoto`. Any other value would simply use the default
 #     behaviour.
 
-: ${TKN_CHAINS_FORMAT:=""}
+: "${TKN_CHAINS_FORMAT:=""}"
 
 # Install Chains.
-kubectl apply --filename $GIT_ROOT/platform/vendor/tekton/chains/release.yaml
+kubectl apply --filename "$GIT_ROOT"/platform/vendor/tekton/chains/release.yaml
 
 kubectl patch \
       configmap chains-config \
       -n tekton-chains \
-      --patch-file $GIT_ROOT/platform/components/tekton/chains/patch_config_oci.yaml
+      --patch-file "$GIT_ROOT"/platform/components/tekton/chains/patch_config_oci.yaml
 
 # Patch chains to generate in-toto provenance.
 case "${TKN_CHAINS_FORMAT}" in
@@ -27,7 +27,7 @@ case "${TKN_CHAINS_FORMAT}" in
     kubectl patch \
       configmap chains-config \
       -n tekton-chains \
-      --patch-file $GIT_ROOT/platform/components/tekton/chains/patch_config_intoto.yaml
+      --patch-file "$GIT_ROOT"/platform/components/tekton/chains/patch_config_intoto.yaml
     ;;
   *)
     ;;

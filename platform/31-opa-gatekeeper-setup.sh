08d371fa9d54ee12964f4b497a25c6c682ff764f
Joshua Lucas <jlucas5190@gmail.com>
2021-11-15 09:37:01 -0600

    shellcheck lint Double quote to prevent globbing and word splitting.

--- platform/31-opa-gatekeeper-setup.sh
+++ platform/31-opa-gatekeeper-setup.sh
@@ -24,14 +24,14 @@ cleanup_cluster(){
 
 install_opa_gatekeeper(){
     helm repo add gatekeeper https://open-policy-agent.github.io/gatekeeper/charts
-    helm upgrade -f ${SCRIPT_DIR}/../resources/opa-gatekeeper/enable-mutating.yaml --install gatekeeper gatekeeper/gatekeeper --version $OPA_GATEKEEPER_VERSION
+    helm upgrade -f "${SCRIPT_DIR}"/../resources/opa-gatekeeper/enable-mutating.yaml --install gatekeeper gatekeeper/gatekeeper --version $OPA_GATEKEEPER_VERSION
 }
 
 
 install_secrets(){
     # Create secrets for gatekeeper/cosign wrapper api and kubernetes image pull
     kubectl create namespace gatekeeper --dry-run=client -o yaml | kubectl apply -f -
-    kubectl create secret generic regcred --type=kubernetes.io/dockerconfigjson --from-file=.dockerconfigjson=$DOCKER_CONFIG_JSON -n gatekeeper  --dry-run=client -o yaml | kubectl apply -f -
+    kubectl create secret generic regcred --type=kubernetes.io/dockerconfigjson --from-file=.dockerconfigjson="$DOCKER_CONFIG_JSON" -n gatekeeper  --dry-run=client -o yaml | kubectl apply -f -
 }
 
 

38d9c3d3887ab66cc8e8f7e816b1a02cb97ce54a
Brad Beck <bradley.beck@gmail.com>
2021-11-30 16:50:28 -0600

    Apply missing git-clone task
    The installation of this task was recently removed from 10-tekton-setup.sh

--- examples/ibm-tutorial/ibm-tutorial.sh
+++ examples/ibm-tutorial/ibm-tutorial.sh
@@ -5,6 +5,7 @@ GIT_ROOT=$(git rev-parse --show-toplevel)
 
 # This IBM tutorial looks great:
 #   https://developer.ibm.com/devpractices/devops/tutorials/build-and-deploy-a-docker-image-on-kubernetes-using-tekton-pipelines/#create-a-task-to-clone-the-git-repository
+kubectl apply -f "${GIT_ROOT}"/platform/vendor/tekton/catalog/main/task/git-clone/0.4/git-clone.yaml
 kubectl apply -f https://raw.githubusercontent.com/tektoncd/chains/main/examples/kaniko/kaniko.yaml
 kubectl apply -f "$GIT_ROOT"/examples/ibm-tutorial/pipeline-pvc.yaml
 kubectl apply -f "$GIT_ROOT"/examples/ibm-tutorial/pipeline-account.yaml

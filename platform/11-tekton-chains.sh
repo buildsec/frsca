2ca78cca6ec9e8650c823b129fe49e0e44b08308
Parth Patel <88045217+pxp928@users.noreply.github.com>
2022-01-12 15:42:57 -0500

    removed unused chains patch. Using in-toto and OCI (#117)
    * removed unused chains patch. Using in-toto and OCI
    
    * Hack to vendor chains by sha256 (#4)
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>
    Co-authored-by: Brad Beck <bradley.beck@gmail.com>

--- platform/11-tekton-chains.sh
+++ platform/11-tekton-chains.sh
@@ -3,33 +3,13 @@ set -euo pipefail
 
 GIT_ROOT=$(git rev-parse --show-toplevel)
 
-# Setup tekton Chains and install Cosign if needed.
-#
-# Environment variables:
-#   TKN_CHAINS_FORMAT: defines an alternative format to store provenance
-#     information.
-#     Possibles values: `intoto`. Any other value would simply use the default
-#     behaviour.
-
-: "${TKN_CHAINS_FORMAT:=""}"
+# Setup tekton Chains
 
 # Install Chains.
 kubectl apply --filename "$GIT_ROOT"/platform/vendor/tekton/chains/release.yaml
 
+# Patch chains to generate in-toto provenance and store output in OCI
 kubectl patch \
       configmap chains-config \
       -n tekton-chains \
       --patch-file "$GIT_ROOT"/platform/components/tekton/chains/patch_config_oci.yaml
-
-# Patch chains to generate in-toto provenance.
-case "${TKN_CHAINS_FORMAT}" in
-  intoto)
-    kubectl patch \
-      configmap chains-config \
-      -n tekton-chains \
-      --patch-file "$GIT_ROOT"/platform/components/tekton/chains/patch_config_intoto.yaml
-    ;;
-  *)
-    ;;
-esac
-

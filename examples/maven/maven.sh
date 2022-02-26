fc670213ef234b175488b3c3e715c3ba3504e76d
Annie pamela Kwengoua nkuinguen <kwengouapamela@yahoo.com>
2022-02-26 11:18:20 -0600

    reviewed feedback

--- examples/maven/maven.sh
+++ examples/maven/maven.sh
@@ -6,7 +6,7 @@ GIT_ROOT=$(git rev-parse --show-toplevel)
 DEFAULT_REPOSITORY=$(xxd -l 16 -c 16 -p < /dev/random)
 : "${REGISTRY:=ttl.sh}"
 : "${REPOSITORY:=$REGISTRY/$DEFAULT_REPOSITORY}"
-C_GREEN'\033[32m'
+C_GREEN='\033[32m'
 C_RESET_ALL='\033[0m'
 
 # Install shared tasks.

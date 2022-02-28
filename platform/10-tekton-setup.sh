adce541777d4d3af059075848f0bd131463e6b5b
Brad Beck <bradley.beck@gmail.com>
2022-02-28 13:42:44 -0600

    Vendor tekton dashboard v0.24.1 (#144)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- platform/10-tekton-setup.sh
+++ platform/10-tekton-setup.sh
@@ -16,7 +16,7 @@ kubectl rollout status -n tekton-pipelines deployment/tekton-pipelines-controlle
 #   Use `kubectl proxy --port=8080` and then
 #   http://localhost:8080/api/v1/namespaces/tekton-pipelines/services/tekton-dashboard:http/proxy/
 #   to access it.
-kubectl apply --filename https://github.com/tektoncd/dashboard/releases/latest/download/tekton-dashboard-release.yaml
+kubectl apply --filename "$GIT_ROOT"/platform/vendor/tekton/dashboard/tekton-dashboard-release.yaml
 kubectl rollout status -n tekton-pipelines deployment/tekton-dashboard
 
 # Wait for tekton pipelines configuration webhook to come up

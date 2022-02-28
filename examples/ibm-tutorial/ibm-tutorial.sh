790d93b78086a9e030c36686d5088e0e8ca2a8b7
Brad Beck <bradley.beck@gmail.com>
2022-02-28 11:25:43 -0600

    Make the examples more consistent (#146)
    - Change `ibm-tutorial` to use the same repository setting scheme as the other examples
    - Allow the instructions in the quick start guide to work for all the examples, including `buildpacks`
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- examples/ibm-tutorial/ibm-tutorial.sh
+++ examples/ibm-tutorial/ibm-tutorial.sh
@@ -3,15 +3,18 @@ set -euo pipefail
 
 # Define variables.
 GIT_ROOT=$(git rev-parse --show-toplevel)
+DEFAULT_REPOSITORY=$(xxd -l 16 -c 16 -p < /dev/random)
+: "${REGISTRY:=ttl.sh}"
+: "${REPOSITORY:=$REGISTRY/$DEFAULT_REPOSITORY}"
 C_GREEN='\033[32m'
 C_RESET_ALL='\033[0m'
 
 # Create the IBM tutorial pipelinerun.
-echo -e "${C_GREEN}Creating a IBM tutorial pipelinerun...${C_RESET_ALL}"
+echo -e "${C_GREEN}Creating a IBM tutorial pipelinerun: REPOSITORY=${REPOSITORY}${C_RESET_ALL}"
 kubectl apply -f "${GIT_ROOT}"/platform/vendor/tekton/catalog/main/task/git-clone/0.4/git-clone.yaml
 kubectl apply -f "${GIT_ROOT}"/platform/vendor/tekton/catalog/main/task/kaniko/kaniko.yaml
 pushd "${GIT_ROOT}"
-cue apply ./examples/ibm-tutorial | kubectl apply -f -
-cue create ./examples/ibm-tutorial | kubectl create -f -
+cue -t "repository=${REPOSITORY}" apply ./examples/ibm-tutorial | kubectl apply -f -
+cue -t "repository=${REPOSITORY}" create ./examples/ibm-tutorial | kubectl create -f -
 popd
 tkn pipelinerun describe --last

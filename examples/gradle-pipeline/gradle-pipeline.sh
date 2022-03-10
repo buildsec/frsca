8277b26f524d46dcf7fce015a747c7a5600b384e
Joshua Lucas <jlucas5190@gmail.com>
2022-03-10 16:37:56 -0600

    addition of IMAGE_URL and write url to jib-gradle task

--- examples/gradle-pipeline/gradle-pipeline.sh
+++ examples/gradle-pipeline/gradle-pipeline.sh
@@ -11,7 +11,7 @@ C_RESET_ALL='\033[0m'
 
 # Install shared tasks.
 kubectl apply -f "${GIT_ROOT}"/platform/vendor/tekton/catalog/main/task/git-clone/0.4/git-clone.yaml
-kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/jib-gradle/0.3/jib-gradle.yaml
+kubectl apply -f "${GIT_ROOT}"/platform/vendor/tekton/catalog/main/task/gradle/0.3/jib-gradle.yaml
 kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/trivy-scanner/0.1/trivy-scanner.yaml
 kubectl apply -f "${GIT_ROOT}"/platform/vendor/tekton/catalog/main/task/kaniko/kaniko.yaml
 

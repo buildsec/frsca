1ffcc390b44dbcfdaae7ab1e5927662882b2e8f9
Brad Beck <bradley.beck@gmail.com>
2022-03-24 09:29:22 -0500

    Use buildpacks tasks/pipelines from the same repository

--- examples/buildpacks/buildpacks.sh
+++ examples/buildpacks/buildpacks.sh
@@ -11,11 +11,11 @@ C_RESET_ALL='\033[0m'
 
 # Install shared tasks.
 kubectl apply -f "${GIT_ROOT}"/platform/vendor/tekton/catalog/main/task/git-clone/0.4/git-clone.yaml
-kubectl apply -f https://raw.githubusercontent.com/buildpacks/tekton-integration/main/task/buildpacks/0.4/buildpacks.yaml
-kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/buildpacks-phases/0.2/buildpacks-phases.yaml
+kubectl apply -f "${GIT_ROOT}"/platform/vendor/buildpacks/tekton-integration/main/task/buildpacks/0.4/buildpacks.yaml
+kubectl apply -f "${GIT_ROOT}"/platform/vendor/buildpacks/tekton-integration/main/task/buildpacks-phases/0.2/buildpacks-phases.yaml
 
 # Install shared pipeline.
-kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/pipeline/buildpacks/0.1/buildpacks.yaml
+kubectl apply -f "${GIT_ROOT}"/platform/vendor/buildpacks/tekton-integration/main/pipeline/buildpacks/0.1/buildpacks.yaml
 
 # Install the buildpacks pipelinerun.
 echo -e "${C_GREEN}Creating a buildpacks pipelinerun: REPOSITORY=${REPOSITORY}${C_RESET_ALL}"

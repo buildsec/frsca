9fb9d3b0068253ef918170ad00b1fb109fd68a98
pxp928 <parth.psu@gmail.com>
2022-07-05 16:35:10 -0400

    adding missing folder
    Signed-off-by: pxp928 <parth.psu@gmail.com>

--- platform/07-tetragon-setup.sh
+++ platform/07-tetragon-setup.sh
@@ -16,5 +16,5 @@ helm upgrade --install tetragon "${GIT_ROOT}/platform/vendor/tetragon/chart" \
 kubectl rollout status -n kube-system daemonset/tetragon
 
 echo -e "${C_GREEN}Enable tracing policies for Tetragon...${C_RESET_ALL}"
-kubectl apply -f "${GIT_ROOT}/platform/vendor/tetragon/tracingpolicy/sys_write_follow_fd_prefix.yaml"
-kubectl apply -f "${GIT_ROOT}/platform/vendor/tetragon/tracingpolicy/tcp-connect.yaml"
+kubectl apply -f "${GIT_ROOT}/platform/vendor/tetragon/tracingpolicy/v0.8.0/sys_write_follow_fd_prefix.yaml"
+kubectl apply -f "${GIT_ROOT}/platform/vendor/tetragon/tracingpolicy/v0.8.0/tcp-connect.yaml"

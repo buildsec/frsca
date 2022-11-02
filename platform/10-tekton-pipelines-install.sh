216ed149b91bc7628035dbeed8de641d2a826fee
Brad Beck <bradley.beck@gmail.com>
2022-11-02 14:09:56 -0500

    Drop Tekton Dashboard
    Signed-off-by: Brad Beck <bradley.beck@gmail.com>

--- platform/10-tekton-pipelines-install.sh
+++ platform/10-tekton-pipelines-install.sh
@@ -15,15 +15,6 @@ kubectl apply --filename "$GIT_ROOT"/platform/vendor/tekton/triggers/interceptor
 
 kubectl apply --filename "${GIT_ROOT}/platform/components/tekton/triggers/rbac.yaml"
 
-# Setup the Dashboard.
-#   Use `kubectl proxy --port=8080` and then
-#   http://localhost:8080/api/v1/namespaces/tekton-pipelines/services/tekton-dashboard:http/proxy/
-#   to access it.
-
-echo -e "${C_GREEN}Installing up Tekton Dashboard...${C_RESET_ALL}"
-kubectl apply --filename "$GIT_ROOT"/platform/vendor/tekton/dashboard/tekton-dashboard-release.yaml
-kubectl rollout status -n tekton-pipelines deployment/tekton-dashboard
-
 # Wait for tekton deployments to finish
 for deployment in tekton-pipelines-webhook tekton-pipelines-controller tekton-triggers-controller tekton-triggers-core-interceptors tekton-triggers-webhook; do
   kubectl rollout status -n tekton-pipelines "deployment/${deployment}"

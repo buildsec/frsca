3f9deeee4f96d870d7b71fcceacc8e664e154c99
pxp928 <parth.psu@gmail.com>
2022-12-16 11:59:14 -0500

    bump kyverno to the latest version
    Signed-off-by: pxp928 <parth.psu@gmail.com>

--- platform/30-kyverno-install.sh
+++ platform/30-kyverno-install.sh
@@ -12,7 +12,10 @@ C_RESET_ALL='\033[0m'
 #   https://nirmata.com/2021/08/12/kubernetes-supply-chain-policy-management-with-cosign-and-kyverno/
 #   Installation: https://kyverno.io/docs/installation/
 
+echo -e "${C_GREEN}Delete Kyverno if already installed...${C_RESET_ALL}"
+kubectl delete -f "$KYVERNO_INSTALL_DIR"/install.yaml || true
+
 echo -e "${C_GREEN}Installing Kyverno...${C_RESET_ALL}"
-kubectl apply -f "$KYVERNO_INSTALL_DIR"/install.yaml
+kubectl create -f "$KYVERNO_INSTALL_DIR"/install.yaml
 # Wait for kyverno deployment to complete
 kubectl rollout status -n kyverno deployment/kyverno

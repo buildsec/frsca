7e96aeeb1522024ae332e95a82686642c63e2288
Brad Beck <bradley.beck@gmail.com>
2023-02-22 10:47:04 -0600

    Fix kyverno upgrade
    Fixes: https://github.com/buildsec/frsca/issues/409
    
    Signed-off-by: Brad Beck <bradley.beck@gmail.com>

--- platform/30-kyverno-install.sh
+++ platform/30-kyverno-install.sh
@@ -13,6 +13,6 @@ C_RESET_ALL='\033[0m'
 #   Installation: https://kyverno.io/docs/installation/
 
 echo -e "${C_GREEN}Installing Kyverno...${C_RESET_ALL}"
-kubectl apply --server-side=true -f "$KYVERNO_INSTALL_DIR"/install.yaml
+kubectl apply --server-side=true --force-conflicts -f "$KYVERNO_INSTALL_DIR"/install.yaml
 # Wait for kyverno deployment to complete
 kubectl rollout status -n kyverno deployment/kyverno

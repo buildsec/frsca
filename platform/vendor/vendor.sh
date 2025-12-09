ea9158b75473dada545ef593a44c39f356f808c9
Brad Beck <bradley.beck@gmail.com>
2025-12-09 13:14:45 -0600

    Update vendorme
    Signed-off-by: Brad Beck <bradley.beck@gmail.com>

--- platform/vendor/vendor.sh
+++ platform/vendor/vendor.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 GIT_ROOT=$(git rev-parse --show-toplevel)
 
-go install github.com/buildsec/vendorme@cee93cf3a994e0cf1f8b1e3d69fc4aaa144f2173
+go install github.com/buildsec/vendorme@109f06dc20d01277f938a3a9fdd2f14f1c32ce2c
 
 pushd "$GIT_ROOT/platform/vendor"
 vendorme pull
